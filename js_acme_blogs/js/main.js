c02e4e68493223a227c414bcd4a9726f7a0b6b7e
noreschnick-boop <noreschnick@gmail.com>
2025-11-25 11:24:53 -0700

    Add files via upload

---
+++ js_acme_blogs/js/main.js
@@ -0,0 +1,19 @@
+function createElemWithText(element, textContent) {
+    let element = "p";
+    let textContent = "";
+    let createdElement = document.createElement(element);
+    createdElement.textContent = textContent;
+    return createdElement;
+};
+
+function createSelectOptions() {
+
+};
+
+function toggleCommentSection() {
+
+};
+
+function toggleCommentButton() {
+
+};
